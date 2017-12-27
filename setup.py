fffd52c26ddb3b390d1322ac46300ce720649460
Florian Mounier <florian.mounier@kozea.fr>
2017-12-27 14:51:15 +0100

    Update python dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ __version__ = "0.0.1"
 
 tests_requirements = [
     'pytest-runner == 3.0', 'pytest-cov == 2.5.1', 'pytest-flake8 == 0.9.1',
-    'pytest-isort == 0.1.0', 'pytest == 3.2.5'
+    'pytest-isort == 0.1.0', 'pytest == 3.3.1'
 ]
 
 setup(
@@ -26,7 +26,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "Flask == 0.12.2", "SQLAlchemy == 1.1.15", "psycopg2 == 2.7.3.2",
-        "unrest == 0.6.7"
+        "unrest == 0.7.1"
     ],
     provides=["apparatus"],
     setup_requires=['pytest-runner'],
