410fc2b87a847f9b064e1e79ad8afeb861aa6805
Florian Mounier <florian.mounier@kozea.fr>
2018-02-27 16:51:06 +0100

    And python deps

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ if 'register' in sys.argv or 'upload' in sys.argv:
 __version__ = "0.0.1"
 
 tests_requirements = [
-    'pytest-runner == 3.0', 'pytest-cov == 2.5.1', 'pytest-flake8 == 0.9.1',
-    'pytest-isort == 0.1.0', 'pytest == 3.3.1'
+    'pytest-runner == 4.0', 'pytest-cov == 2.5.1', 'pytest-flake8 == 0.9.1',
+    'pytest-isort == 0.1.0', 'pytest == 3.4.1'
 ]
 
 setup(
@@ -25,8 +25,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "Flask == 0.12.2", "SQLAlchemy == 1.2.0", "psycopg2 == 2.7.3.2",
-        "unrest == 0.7.1"
+        "Flask == 0.12.2", "SQLAlchemy == 1.2.4", "psycopg2-binary == 2.7.4",
+        "unrest == 0.7.4"
     ],
     provides=["apparatus"],
     setup_requires=['pytest-runner'],
