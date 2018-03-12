93f5f97b270dfd1cdc202bf465b9dfd8349a957e
Florian Mounier <florian.mounier@kozea.fr>
2018-03-12 11:46:43 +0100

    Deps

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ __version__ = "0.0.1"
 
 tests_requirements = [
     'pytest-runner == 4.0', 'pytest-cov == 2.5.1', 'pytest-flake8 == 0.9.1',
-    'pytest-isort == 0.1.0', 'pytest == 3.4.1'
+    'pytest-isort == 0.1.0', 'pytest == 3.4.2'
 ]
 
 setup(
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "Flask == 0.12.2", "SQLAlchemy == 1.2.4", "psycopg2-binary == 2.7.4",
+        "Flask == 0.12.2", "SQLAlchemy == 1.2.5", "psycopg2-binary == 2.7.4",
         "unrest == 0.7.4"
     ],
     provides=["apparatus"],
