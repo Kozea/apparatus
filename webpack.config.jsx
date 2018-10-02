ae5eb3ab9363e6f87a6d6a7622cdb5d6285f4887
Florian Mounier <florian.mounier@kozea.fr>
2018-10-02 14:26:44 +0200

    Update deps and fix old browsers

--- webpack.config.jsx
+++ webpack.config.jsx
@@ -9,8 +9,9 @@ import { renderHtml } from './lib/frontend/src/render.jsx'
 const webpackConfig = getBaseConfig(config, renderHtml)
 
 webpackConfig.module.rules[0].include = [
-  ...webpackConfig.module.rules[0].include,
+  webpackConfig.module.rules[0].include,
   path.join(config.dirs.modules, 'formol'),
+  path.join(config.dirs.modules, 'redux-api-unrest'),
 ]
 
 export default webpackConfig
